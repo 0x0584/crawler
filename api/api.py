0dd3184cf3c66de465b9dc74a8287b961d75c7f7
Anas Rchid <archid-@student.1337.ma>
2020-02-14 02:29:59 +0100

    clean api.py and add more routes

--- api/api.py
+++ api/api.py
@@ -6,24 +6,33 @@
 #    By: archid- <archid-@student.1337.ma>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/14 00:54:32 by archid-           #+#    #+#              #
-#    Updated: 2020/02/14 02:05:21 by archid-          ###   ########.fr        #
+#    Updated: 2020/02/14 02:29:36 by archid-          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-from flask import Flask, request, jsonify, make_response, Response
+from flask import Flask, request
 from bson import json_util
 from pymongo import MongoClient
 
 import json
 
-app = Flask(__name__)
+app     = Flask(__name__)
+conn    = MongoClient()
+db      = conn.crawler_db
 
-conn = MongoClient()
-db = conn.crawler_db
+def json_response(blob):
+    response = app.response_class(
+        response=json.dumps(blob,default=json_util.default),
+        status=200, mimetype='application/json')
+    return response
+
+def do_query(field, item):
+    results = db['Articles'].find({field: {'$regex': item, '$options' : 'i'}})
+    return json_response([foo for foo in results])
 
 @app.route('/', methods=['GET'])
 def index():
-    json.dumps({})
+    return json_response({})
 
 @app.route('/word/<w>', methods=['GET'])
 def word(w):
@@ -35,13 +44,15 @@ def tag(t):
     if request.method == 'GET':
         return do_query('tags', t)
 
-def do_query(field, item):
-    results = db['Articles'].find({field: {'$regex': item, '$options' : 'i'}})
-    json_res = [foo for foo in results]
-    response = app.response_class(
-        response=json.dumps(json_res,default=json_util.default),
-        status=200, mimetype='application/json')
-    return response
+@app.route('/author/<a>', methods=['GET'])
+def author(a):
+    if request.method == 'GET':
+        return do_query('author', a)
+
+@app.route('/text/<a>', methods=['GET'])
+def text(a):
+    if request.method == 'GET':
+        return do_query('body', a)
 
 if __name__ == "__main__":
     app.run(debug=True)
